b72ec8554d1a2691cfa1d73e2aa0a1efb2672891
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2024-01-18 11:18:09 -0600

    update to webpack, with required functional code

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -4,26 +4,65 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const workboxPlugin = new InjectManifest({
+  swSrc: './src/service-worker.js',
+  swDest: 'service-worker.js',
+});
 
+// TODO: Add CSS loaders and babel to webpack.
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        chunks: ['main'],
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/install.html',
+        filename: 'install.html',
+        chunks: ['install'],
+      }),
+      new WebpackPwaManifest({
+        name: 'My PWA',
+        short_name: 'PWA',
+        description: 'My Progressive Web App',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: path.resolve('src/assets/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+      workboxPlugin,
     ],
-
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
